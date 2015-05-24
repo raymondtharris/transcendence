2e9d9ac068a6d14fabd5e998aae52f8faffcf679
Tim <raymondtimothyharris@gmail.com>
2015-05-24 00:03:06 -0700

    Started Project
    Started iOS part of project and first Class Octalye.

---
+++ transcendence/transcendence/Octalye.swift
@@ -0,0 +1,16 @@
+//
+//  Octalye.swift
+//  transcendence
+//
+//  Created by Tim Harris on 5/23/15.
+//  Copyright (c) 2015 Tim Harris. All rights reserved.
+//
+
+import Foundation
+
+class Octalye: NSObject {
+    var Name: String
+    init(newName: String) {
+        self.Name = newName
+    }
+}
