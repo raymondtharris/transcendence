b6bb2bf2b8156b19d55e6b5cdb90c399f5bb45d2
Tim Harris <raymondtimothyharris@gmail.com>
2016-07-20 22:12:08 -0700

    GameLoadViewController Tableview
    Added a tableview to this view controller to unify the three saves that
    will appear for the user to load from. Trying to make the UI more
    cohesive.

---
+++ transcendence/transcendence/GameLoadViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameLoadViewController.swift
+//  transcendence
+//
+//  Created by Tim Harris on 7/20/16.
+//  Copyright © 2016 Tim Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameLoadViewController: UIViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func backToTitleScrean(segue: UIStoryboardSegue) {
+        
+    }
+    
+    func askToLoadGameSave(saveToAskFor: Int) {
+        
+    }
+}
+
+class GameSaveView: UIView {
+    
+
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+    }
+}
